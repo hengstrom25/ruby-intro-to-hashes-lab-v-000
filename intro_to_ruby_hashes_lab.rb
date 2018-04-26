37eb3c14a3a93fb77a7d6b40814fdab62b6b3ba5
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-26 17:16:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,17 +13,14 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {railroads: 
-	{pieces: 4}
-	  }
+	{pieces: 4, 
+	  names: {}
+	 }
+	}
 end
 
 def monopoly_with_third_tier
-	monopoly = {
-	  railroads: {
-	    pieces: 4, 
-	    names: {}
-	  }
-	}
+
 end
 
 def monopoly_with_fourth_tier
